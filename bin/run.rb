547c3f82eed417371c13b6b6d36e8c4079ea19d5
Chester Lam <chesterlam620@gmail.com>
2015-12-10 07:48:11 +1100

    Add run file in bin/

---
+++ bin/run.rb
@@ -0,0 +1,18 @@
+require 'require_all'
+
+require_all 'lib'
+
+calculator = PayslipProcessor.new
+
+employees = calculator.process_csv
+
+payslips = calculator.create_payslips(employees)
+
+puts 'Name,Pay Period,Gross Income,Income Tax,Net Income,Super'
+
+payslips.each do |payslip|
+  puts "#{payslip.full_name},#{payslip.pay_period},"\
+  "#{payslip.gross_income},#{payslip.income_tax},"\
+  "#{payslip.net_income},#{payslip.super_amount}"
+end
+
